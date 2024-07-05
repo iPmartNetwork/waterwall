eeba71ff0af3a1386b30010e4646695ec827708c
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-07-06 02:46:29 +0330

    Update hd.sh

--- hd.sh
+++ hd.sh
@@ -23,202 +23,182 @@ BLUE='\033[0;34m'
 MAGENTA='\033[0;35m'
 NC='\033[0m' # No Color
 
-# Determine the architecture and set the ASSET_NAME accordingly
-ARCH=$(uname -m)
-if [ "$ARCH" == "aarch64" ]; then
-  ASSET_NAME="Waterwall-linux-arm64.zip"
-elif [ "$ARCH" == "x86_64" ]; then
-  ASSET_NAME="Waterwall-linux-64.zip"
-else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-fi
+cur_dir=$(pwd)
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${RED}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
 
-# Function to download and unzip the release
-download_and_unzip() {
-  local url="$1"
-  local dest="$2"
-
-  echo "Downloading $dest from $url..."
-  wget -q -O "$dest" "$url"
-  if [ $? -ne 0 ]; then
-    echo "Error: Unable to download file."
-    return 1
-  fi
-
-  echo "Unzipping $dest..."
-  unzip -o "$dest"
-  if [ $? -ne 0 ]; then
-    echo "Error: Unable to unzip file."
-    return 1
-  fi
-  
-  sleep 0.5
-  chmod +x Waterwall
-  rm "$dest"
-
-  echo "Download and unzip completed successfully."
-}
-
-# Function to get download URL for the latest release
-get_latest_release_url() {
-  local api_url="https://api.github.com/repos/radkesvat/WaterWall/releases/latest"
-
-  echo "Fetching latest release data..." >&2
-  local response=$(curl -s "$api_url")
-  if [ $? -ne 0 ]; then
-    echo "Error: Unable to fetch release data." >&2
-    return 1
-  fi
-
-  local asset_url=$(echo "$response" | jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
-  if [ -z "$asset_url" ]; then
-    echo "Error: Asset not found." >&2
-    return 1
-  fi
-
-  echo "$asset_url"
-}
-
-# Function to get download URL for a specific release version
-get_specific_release_url() {
-  local version=$1
-  local api_url="https://api.github.com/repos/radkesvat/WaterWall/releases/tags/$version"
-
-  echo "Fetching release data for version $version..." >&2
-  response=$(curl -s $api_url)
-  if [ $? -ne 0 ]; then
-    echo "Error: Unable to fetch release data for version $version." >&2
-    exit 1
-  fi
-
-  local asset_url=$(echo $response | jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
-  if [ -z "$asset_url" ]; then
-    echo "Error: Asset not found for version $version." >&2
-    exit 1
-  fi
-
-  echo $asset_url
-}
-
-setup_waterwall_service() {
-    cat > /etc/systemd/system/waterwall.service << EOF
-[Unit]
-Description=Waterwall Service
-After=network.target
-
-[Service]
-ExecStart=/root/RRT/Waterwall
-WorkingDirectory=/root/RRT
-Restart=always
-RestartSec=5
-User=root
-StandardOutput=null
-StandardError=null
-
-[Install]
-WantedBy=multi-user.target
-EOF
-    systemctl daemon-reload
-    systemctl enable waterwall
-    systemctl start waterwall
-}
-
-while true; do
-    echo -e "${Purple}Select an option:${NC}"
-    echo -e "${White}1. IRAN ${NC}"
-    echo -e "${Cyan}2. KHAREJ ${NC}"
-    echo -e "${White}3. Uninstall${NC}"
-    echo -e "${Cyan}0. Exit ${NC}"
-
-    read -p "Enter your choice: " choice
-    if [[ "$choice" -eq 1 || "$choice" -eq 2 ]]; then
-        SSHD_CONFIG_FILE="/etc/ssh/sshd_config"
-        CURRENT_PORT=$(grep -E '^(#Port |Port )' "$SSHD_CONFIG_FILE")
-
-        if [[ "$CURRENT_PORT" != "Port 22" && "$CURRENT_PORT" != "#Port 22" ]]; then
-            sudo sed -i -E 's/^(#Port |Port )[0-9]+/Port 22/' "$SSHD_CONFIG_FILE"
-            echo "SSH Port has been updated to Port 22."
-            sudo systemctl restart sshd
-            sudo service ssh restart
-        fi
-        sleep 0.5
-        mkdir /root/RRT
-        cd /root/RRT
-        apt install unzip -y
-        apt install jq -y
-
-        read -p "Do you want to install the latest version? (y/n): " answer
-        if [[ "$answer" == [Yy]* ]]; then
-            # Get the latest release URL
-            url=$(get_latest_release_url)
-
-            if [ $? -ne 0 ] || [ -z "$url" ]; then
-                echo "Failed to retrieve the latest release URL."
-                exit 1
-            fi
-            echo "Latest Release URL: $url"
-            download_and_unzip "$url" "$ASSET_NAME"
-            if [ $? -ne 0 ]; then
-                echo "Failed to download or unzip the file."
-                exit 1
-            fi
-        elif [[ "$answer" == [Nn]* ]]; then
-            read -p "Enter the version you want to install (e.g., v1.18): " version
-            # Get the specific release URL
-            url=$(get_specific_release_url "$version")
-
-            if [ $? -ne 0 ] || [ -z "$url" ]; then
-                echo "Failed to retrieve the latest release URL."
-                exit 1
-            fi
-            echo "Specific Version URL: $url"
-            download_and_unzip "$url" "$ASSET_NAME"
+install_jq() {
+    if ! command -v jq &> /dev/null; then
+        # Check if the system is using apt package manager
+        if command -v apt-get &> /dev/null; then
+            echo -e "${RED}jq is not installed. Installing...${NC}"
+            sleep 1
+            sudo apt-get update
+            sudo apt-get install -y jq
         else
-            echo "Please answer yes (y) or no (n)."
-            break
+            echo -e "${RED}Error: Unsupported package manager. Please install jq manually.${NC}\n"
+            read -p "Press any key to continue..."
+            exit 1
         fi
-
-        cat > core.json << EOF
-{
-    "log": {
-        "path": "log/",
-        "core": {
-            "loglevel": "DEBUG",
-            "file": "core.log",
-            "console": true
-        },
-        "network": {
-            "loglevel": "DEBUG",
-            "file": "network.log",
-            "console": true
-        },
-        "dns": {
-            "loglevel": "SILENT",
-            "file": "dns.log",
-            "console": false
-        }
-    },
-    "dns": {},
-    "misc": {
-        "workers": 0,
-        "ram-profile": "server",
-        "libs-path": "libs/"
-    },
-    "configs": [
-        "config.json"
-    ]
-}
-EOF
     fi
+}
 
-    if [ "$choice" -eq 1 ]; then
-        public_ip=$(wget -qO- https://api.ipify.org)
-        echo -e "${Cyan}You chose Iran.${NC}"
-        read -p "enter Kharej Ipv4: " ip_remote
-        read -p "Enter the SNI (default: ipmart.shop): " input_sni
-        HOSTNAME=${input_sni:-ipmart.shop}
-        cat > config.json << EOF
+
+loader(){
+
+    install_jq
+
+    # Get server IP
+    SERVER_IP=$(hostname -I | awk '{print $1}')
+
+    # Fetch server country using ip-api.com
+    SERVER_COUNTRY=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.country')
+
+    # Fetch server isp using ip-api.com 
+    SERVER_ISP=$(curl -sS "http://ip-api.com/json/$SERVER_IP" | jq -r '.isp')
+
+    WATER_CORE=$(check_core_status)
+    WATER_TUNNEL=$(check_tunnel_status)
+
+    init
+
+}
+
+init(){
+
+    #clear page .
+    clear
+    echo "+--------------------------------------------------------------------------------------------------------------+"
+    echo "|                                                                                                              |" 
+    echo "| __          __        _               __          __        _  _   _______                             _     |" 
+    echo "| \ \        / /       | |              \ \        / /       | || | |__   __|                           | |    |" 
+    echo "|  \ \  /\  / /   __ _ | |_   ___  _ __  \ \  /\  / /   __ _ | || |    | |    _   _  _ __   _ __    ___ | |    |" 
+    echo "|   \ \/  \/ /   / _  || __| / _ \| '__|  \ \/  \/ /   / _  || || |    | |   | | | || '_ \ |  _ \  / _ \| |    |" 
+    echo "|    \  /\  /   | (_| || |_ |  __/| |      \  /\  /   | (_| || || |    | |   | |_| || | | || | | ||  __/| |    |" 
+    echo "|     \/  \/     \__,_| \__| \___||_|       \/  \/     \__,_||_||_|    |_|    \__,_||_| |_||_| |_| \___||_|    |" 
+    echo "|                                                                                                              |" 
+    echo "+--------------------------------------------------------------------------------------------------------------+"                                                                                                         
+    echo -e "|${GREEN}Server Country    |${NC} $SERVER_COUNTRY"
+    echo -e "|${GREEN}Server IP         |${NC} $SERVER_IP"
+    echo -e "|${GREEN}Server ISP        |${NC} $SERVER_ISP"
+    echo -e "|${GREEN}WaterWall CORE    |${NC} $WATER_CORE"
+    echo -e "|${GREEN}WaterWall Tunnel  |${NC} $WATER_TUNNEL"
+    echo "+--------------------------------------------------------------------------------------------------------------+"
+    echo -e "|${YELLOW}Please choose an option:${NC}"
+    echo "+--------------------------------------------------------------------------------------------------------------+"
+    echo -e "${YELLOW}| 1  - INSTALL CORE ${NC}"
+    echo -e "${YELLOW}| 2  - Config Tunnel ${NC}"
+    echo -e "${YELLOW}| 3  - Unistall ${NC}"
+    echo -e "${YELLOW}| 0  - Exit ${NC}"
+    echo "+--------------------------------------------------------------------------------------------------------------+"
+    echo -e "\033[0m"
+
+    read -p "Enter option number: " choice
+    case $choice in
+    1)
+        install_core
+        ;;  
+    2)
+        config_tunnel
+        ;;
+    3)
+        unistall
+        ;;
+    0)
+        echo -e "${GREEN}Exiting program...${NC}"
+        exit 0
+        ;;
+    *)
+        echo "Not valid"
+        ;;
+    esac
+        
+
+}
+
+install_core(){
+
+wget https://github.com/radkesvat/WaterWall/releases/download/v1.21/Waterwall-linux-64.zip
+apt install unzip && unzip Waterwall-linux-64.zip
+chmod +rwx Waterwall
+    
+cat <<EOL > core.json
+    {
+        "log": {
+            "path": "log/",
+            "core": {
+                "loglevel": "DEBUG",
+                "file": "core.log",
+                "console": true
+            },
+            "network": {
+                "loglevel": "DEBUG",
+                "file": "network.log",
+                "console": true
+
+            },
+            "dns": {
+                "loglevel": "SILENT",
+                "file": "dns.log",
+                "console": false
+
+            }
+        },
+        "dns": {},
+        "misc": {
+            "workers": 0,
+            "ram-profile": "server",
+            "libs-path": "libs/"
+        },
+        "configs": [
+            "dev-ir.json"
+        ]
+    }
+EOL
+
+    echo 'WaterWall Core installed :)'
+    echo $'\e[32minstalling WaterWall in 3 seconds... \e[0m' && sleep 1 && echo $'\e[32m2... \e[0m' && sleep 1 && echo $'\e[32m1... \e[0m' && sleep 1 && {
+        clear
+        init
+    }
+
+}
+
+config_tunnel(){
+
+        clear
+        echo "+--------------------------------------------------------------------------------------------------------------+"
+        echo "|                                                                                                              |" 
+        echo "| __          __        _               __          __        _  _   _______                             _     |" 
+        echo "| \ \        / /       | |              \ \        / /       | || | |__   __|                           | |    |" 
+        echo "|  \ \  /\  / /   __ _ | |_   ___  _ __  \ \  /\  / /   __ _ | || |    | |    _   _  _ __   _ __    ___ | |    |" 
+        echo "|   \ \/  \/ /   / _  || __| / _ \| '__|  \ \/  \/ /   / _  || || |    | |   | | | || '_ \ |  _ \  / _ \| |    |" 
+        echo "|    \  /\  /   | (_| || |_ |  __/| |      \  /\  /   | (_| || || |    | |   | |_| || | | || | | ||  __/| |    |" 
+        echo "|     \/  \/     \__,_| \__| \___||_|       \/  \/     \__,_||_||_|    |_|    \__,_||_| |_||_| |_| \___||_|    |" 
+        echo "|                                                                                                              |" 
+        echo "+--------------------------------------------------------------------------------------------------------------+"                                                                                                         
+        echo -e "|${GREEN}Server Country    |${NC} $SERVER_COUNTRY"
+        echo -e "|${GREEN}Server IP         |${NC} $SERVER_IP"
+        echo -e "|${GREEN}Server ISP        |${NC} $SERVER_ISP"
+        echo -e "|${GREEN}WaterWall CORE    |${NC} $WATER_CORE"
+        echo -e "|${GREEN}WaterWall Tunnel  |${NC} $WATER_TUNNEL"
+        echo "+--------------------------------------------------------------------------------------------------------------+"
+        echo -e "${GREEN}Please choose an option:${NC}"
+        echo "+---------------------------------------------------------------+"
+        echo -e "${BLUE}| 1  - IRAN"
+        echo -e "${BLUE}| 2  - Kharej"
+        echo -e "${BLUE}| 0  - Exit"
+        echo "+---------------------------------------------------------------+"
+        echo -e "\033[0m"
+
+        read -p "Enter option number: " setup
+        case $setup in
+        1)
+
+            read -p "Enter SNI : " clear_sni
+            read -p "Enter Kharej IP : " kharej_ip
+
+cat <<EOL > dev-ir.json
 {
     "name": "reverse_reality_grpc_hd_multiport_server",
     "nodes": [
@@ -283,7 +263,7 @@ EOF
             "type": "RealityServer",
             "settings": {
                 "destination": "reality_dest",
-                "password": "220049AHS04"
+                "password": "passwd"
             },
             "next": "halfs"
         },
@@ -295,7 +275,7 @@ EOF
                 "port": 443,
                 "nodelay": true,
                 "whitelist": [
-                    "$ip_remote/32"
+                    "$kharej_ip/32"
                 ]
             },
             "next": "reality_server"
@@ -305,28 +285,27 @@ EOF
             "type": "TcpConnector",
             "settings": {
                 "nodelay": true,
-                "address": "$HOSTNAME",
+                "address": "$clear_sni",
                 "port": 443
             }
         }
     ]
 }
-EOF
-        sleep 0.5
-        setup_waterwall_service
-        sleep 0.5
-        echo -e "${Cyan}Iran IPv4 is: $public_ip${NC}"
-        echo -e "${Purple}Kharej IPv4 is: $ip_remote${NC}"
-        echo -e "${Cyan}SNI $HOSTNAME${NC}"
-        echo -e "${Purple}Iran Setup Successfully Created ${NC}"
-        read -p "Press Enter to continue"
-    elif [ "$choice" -eq 2 ]; then
-        public_ip=$(wget -qO- https://api.ipify.org)
-        echo -e "${Purple}You chose Kharej.${NC}"
-        read -p "enter Iran Ip: " ip_remote
-        read -p "Enter the SNI (default: ipmart.shop): " input_sni
-        HOSTNAME=${input_sni:-ipamart.shop}
-        cat > config.json << EOF
+EOL
+            run_screen
+            echo $'\e[32mTunnel WaterWall in 3 seconds... \e[0m' && sleep 1 && echo $'\e[32m2... \e[0m' && sleep 1 && echo $'\e[32m1... \e[0m' && sleep 1 && {
+                clear
+                init
+            }
+            ;;
+        2)
+
+            read -p "Enter SNI : " clear_sni
+            read -p "Enter IRAN IP : " iran_ip
+
+
+
+cat <<EOL > dev-ir.json
 {
     "name": "reverse_reality_grpc_client_hd_multiport_client",
     "nodes": [
@@ -399,8 +378,8 @@ EOF
             "name": "reality_client",
             "type": "RealityClient",
             "settings": {
-                "sni": "$HOSTNAME",
-                "password": "220049AHS04"
+                "sni": "$clear_sni",
+                "password": "passwd"
             },
             "next": "outbound_to_iran"
         },
@@ -409,33 +388,116 @@ EOF
             "type": "TcpConnector",
             "settings": {
                 "nodelay": true,
-                "address": "ip_remote",
+                "address": "$iran_ip",
                 "port": 443
             }
         }
     ]
 }
-EOF
-        sleep 0.5
-        setup_waterwall_service
-        sleep 0.5
-        echo -e "${Purple}Kharej IPv4 is: $public_ip${NC}"
-        echo -e "${Cyan}Iran IPv4 is: $ip_remote${NC}"
-        echo -e "${Purple}SNI $HOSTNAME${NC}"
-        echo -e "${Cyan}Kharej Setup Successfully Created ${NC}"
-        read -p "Press Enter to continue"
-    elif [ "$choice" -eq 3 ]; then
-        sudo systemctl stop waterwall
-        sudo systemctl disable waterwall
-        rm -rf /etc/systemd/system/waterwall.service
-        pkill -f Waterwall
-        rm -rf /root/RRT
-        echo "Removed"
-        read -p "Press Enter to continue"
-    elif [ "$choice" -eq 0 ]; then
-        echo "Exit.."
-        break
+EOL
+
+            run_screen
+            echo $'\e[32mTunnel WaterWall in 3 seconds... \e[0m' && sleep 1 && echo $'\e[32m2... \e[0m' && sleep 1 && echo $'\e[32m1... \e[0m' && sleep 1 && {
+                clear
+                init
+            }
+
+            ;;
+        0)
+            echo -e "${GREEN}Exiting program...${NC}"
+            exit 0
+            ;;
+        *)
+            echo "Not valid"
+            ;;
+        esac
+        
+
+}
+
+unistall(){
+
+    echo $'\e[32mUninstalling WaterWall in 3 seconds... \e[0m' && sleep 1 && echo $'\e[32m2... \e[0m' && sleep 1 && echo $'\e[32m1... \e[0m' && sleep 1 && {
+    rm Waterwall-linux-64.zip
+    rm Waterwall-linux-64.zip*
+    rm Waterwall
+    rm dev-ir.json
+    rm core.json
+    clear
+    echo 'WaterWall Unistalled :(';
+    }
+
+
+    loader
+}
+
+run_screen(){
+#!/bin/bash
+
+# Check if screen is installed
+if ! command -v screen &> /dev/null
+then
+    echo "Screen is not installed. Installing..."
+    
+    # Check the Linux distribution to use the correct package manager
+    if [ -f /etc/redhat-release ]; then
+        # CentOS/RHEL
+        sudo yum install screen -y
+    elif [ -f /etc/debian_version ]; then
+        # Debian/Ubuntu
+        sudo apt-get update
+        sudo apt-get install screen -y
     else
-        echo "Invalid choice. Please try again."
+        echo "Unsupported Linux distribution. Please install screen manually."
+        exit 1
     fi
-done
+    
+    # Verify installation
+    if ! command -v screen &> /dev/null
+    then
+        echo "Failed to install screen. Please install manually."
+        exit 1
+    else
+        echo "Screen has been successfully installed."
+    fi
+else
+    echo "Screen is already installed."
+fi
+
+# Run WaterWall in a new detached screen session
+# screen -d -m -S WaterWall /path/to/WaterWall
+# screen -S dev.ir ./Waterwall
+screen -dmS WaterWal /root/Waterwall
+
+echo "WaterWall has been started in a new screen session."
+
+}
+
+
+check_core_status() {
+    local file_path="core.json"
+    local status
+
+    if [ -f "$file_path" ]; then
+        status="${GREEN}Installed"${NC}
+    else
+        status=${RED}"Not installed"${NC}
+    fi
+
+    echo "$status"
+}
+
+check_tunnel_status() {
+    local file_path="dev-ir.json"
+    local status
+
+    if [ -f "$file_path" ]; then
+        status="${GREEN}Enabled"${NC}
+    else
+        status=${RED}"Disabled"${NC}
+    fi
+
+    echo "$status"
+}
+
+loader
